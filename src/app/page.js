740676d4877535893374743f549aa72b6a670461
sammed.sankonatti <sammed.sankonatti18@gmail.com>
2025-03-21 16:46:59 +0530

    7th project

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <div className={styles.page}>
+        <h2> NextJs Deployment on Azure App service!</h2>
+      </div>
+    </div>
+  );
+}
